189aa187acd283cc6fbd250600bf395f188ee37a
Prospect <806974320@qq.com>
2024-02-17 16:28:05 +0800

    查询+删除

---
+++ src/main/java/com/yupi/usercenterbackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.usercenterbackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.usercenterbackend.model.domain.User;
+
+/**
+* @author Prospect
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2024-02-16 15:16:31
+* @Entity com.yupi.usercenterbackend.model.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/usercenterbackend/model/domain/User.java
@@ -0,0 +1,85 @@
+package com.yupi.usercenterbackend.model.domain;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 
+ * @TableName user
+ */
+@TableName(value ="user")
+@Data
+public class User implements Serializable {
+    /**
+     * 
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 
+     */
+    private String username;
+
+    /**
+     * 
+     */
+    private String userAccount;
+
+    /**
+     * 
+     */
+    private String avatarUrl;
+
+    /**
+     * 
+     */
+    private Integer gender;
+
+    /**
+     * 
+     */
+    private String userPassword;
+
+    /**
+     * 
+     */
+    private String phone;
+
+    /**
+     * 
+     */
+    private String email;
+
+    /**
+     * 
+     */
+    private Integer userStatus;
+
+    /**
+     * 
+     */
+    private Date createTime;
+
+    /**
+     * 
+     */
+    private Date updateTime;
+
+    /**
+     * 
+     */
+    @TableLogic
+    private Integer isDelete;
+
+    /**
+     * 0普通用户 1管理员
+     */
+    private Integer userRole;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
